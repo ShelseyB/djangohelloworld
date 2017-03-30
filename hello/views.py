f3192ac6a7f379673c6b1f98bf49473c8a2ae227
Shelsey Brinkerhoff <shelseyb@gmail.com>
2017-03-30 17:38:44 +0000

    Added app to INSTALLED_APPS, added code to view.py to display visitor count

--- hello/views.py
+++ hello/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import time
+from .models import PageCount
 # Create your views here.
 
 
@@ -8,4 +9,11 @@ import time
 
 
 def index(request):
-    return HttpResponse("Hello, world! It's " + time.strftime('%c'))
+    row, created = PageCount.objects.get_or_create(page = 'index')
+    row.visits = row.visits + 1
+    row.save()
+    if created:
+        words = "You are the 1st visitor!"
+    else:
+        words = "You are visitor #"+str(row.visits)
+    return HttpResponse("Hello, world! It's " + time.strftime('%c') + "\n" + words)
